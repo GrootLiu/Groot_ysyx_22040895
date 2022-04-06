b93fc081b5fa9bdbf8baac947fa81dcca0751830
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-06 21:15:17 +0800

    > sim RTL ysyx_22040000 张三 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:17 up  2:11,  1 user,  load average: 0.09, 0.10, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
-
-int main() {
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated.h"
+#include "Vtop.h"
+int main(int arvc, char** arvg, char** env) {
   printf("Hello, ysyx!\n");
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop top = new Vtop{contextp};
+  while(!contextp->gotFinish())
+  {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->g == a ^ b);
+  }
+  delete top;
+  delete contextp;
   return 0;
 }
