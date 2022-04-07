b21bda2eb170abd3275c1f4c603550f97af4d097
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 21:32:30 +0800

    > sim RTL ysyx_22040000 张三 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:32:30 up  6:20,  3 users,  load average: 0.55, 0.19, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,43 +4,52 @@
 #include "verilated.h"
 #include "Vtop.h"
 
-//用于输出显示波形
+// 用于输出显示波形
 #include "verilated_vcd_c.h"
 
-double sim_time = 10;
+// This is a 64-bit integer to reduce wrap over issues and allow modulus.
+// This is in units of timeprecision used in Verilog(or from --timescale-override)
+vluint_64 main_time = 10;
 
 int main(int argc, char** argv, char** env) {
   printf("Hello, ysyx!\n");
   VerilatedContext* contextp = new VerilatedContext;
 
   contextp->commandArgs(argc, argv);
-  //用于打开VCD记录
+  // 用于打开VCD记录
   contextp->traceEverOn(true);
 
+	// Instantiation of model 
+	// Create model
   Vtop* top = new Vtop{contextp};
   VerilatedVcdC* tfp = new VerilatedVcdC;
   
-  //还没明白什么意思
+  // 还没明白什么意思
   top->trace(tfp, 99);
-  //VCD文件保存位置
+  // VCD文件保存位置
   tfp->open("../wave.vcd");
 
-  //contextp->time()记录仿真时间
+  // contextp->time()记录仿真时间
   while(contextp->time() < sim_time && !contextp->gotFinish())
-  {
+ {
     contextp->timeInc(1);
     int a = rand() & 1;
     int b = rand() & 1;
     top->a = a;
     top->b = b;
+
+		// Evaluate model
     top->eval();
     printf("a = %d, b = %d, f = %d\n", a, b, top->f);
     assert(top->f == a ^ b);
     tfp->dump(contextp->time());
   }
+
+	// Down simulating
   top->final();
   tfp->close();
   delete top;
   delete contextp;
   return 0;
 }
+
