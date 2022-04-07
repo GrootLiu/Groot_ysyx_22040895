666a11f9c8d1e39295d9b09f0835a949ebb147ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 15:54:57 +0800

    > sim RTL ysyx_22040000 张三 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:54:57 up  3:07,  1 user,  load average: 0.57, 0.66, 0.76

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,13 +3,28 @@
 #include <assert.h>
 #include "verilated.h"
 #include "Vtop.h"
+
+//用于输出显示波形
+#include "verilated_vcd_sc.h"
+
+double sim_time = 10;
+
 int main(int argc, char** argv, char** env) {
   printf("Hello, ysyx!\n");
   VerilatedContext* contextp = new VerilatedContext;
+
   contextp->commandArgs(argc, argv);
+  contextp->traceEverOn(true);
+
   Vtop* top = new Vtop{contextp};
-  while(!contextp->gotFinish())
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+
+  top->trace(ftp, 99);
+  tfp->open(./wave.vcd);
+
+  while(contextp->time() < sim_time && !contextp->gotFinish())
   {
+    contextp->timeInc(1);
     int a = rand() & 1;
     int b = rand() & 1;
     top->a = a;
@@ -17,7 +32,9 @@ int main(int argc, char** argv, char** env) {
     top->eval();
     printf("a = %d, b = %d, f = %d\n", a, b, top->f);
     assert(top->f == a ^ b);
+    tfp->dump(contextp->time());
   }
+  delete tfp;
   delete top;
   delete contextp;
   return 0;
