35ed11fc35a17019e46c0fc7e4c05deec4d08813
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-02 10:03:03 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:03:03 up 12:43,  3 users,  load average: 0.13, 0.10, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -133,7 +133,8 @@ void sdb_mainloop() {
   */
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
-    printf("%s\n", *str_end);
+    printf("%s\n",str);
+    printf("%s\n", str_end);
 
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
