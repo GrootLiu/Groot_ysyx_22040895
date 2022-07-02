32f09fd87cc8d139aec514dea31c47bd8c1e6ffa
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-02 22:54:22 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:22 up 22:36,  3 users,  load average: 0.17, 0.09, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -5,13 +5,27 @@
  */
 #include <regex.h>
 
+/* 
+ * token_type is an integer, 
+ * we must ensure the integers corresponding to different types of token are not the same
+ * the begining of tokens' number is 256
+ * LP: Left parenthesis
+ * RP: Right parenthsis
+ */
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ, TK_PLUS, TK_SUB, TK_MULT, TK_DIV, TK_LP, TK_RP
 
   /* TODO: Add more token types */
 
 };
 
+/**
+ * @description: in the framework code, 
+ *               a rule is a 2-tuple consisting of a regular expression and a token type
+ *               rule = regex + token
+ * @return {*}
+ * @use: 
+ */
 static struct rule {
   const char *regex;
   int token_type;
@@ -26,6 +40,7 @@ static struct rule {
   {"==", TK_EQ},        // equal
 };
 
+/* NR_REGEX indicates the number of rules */
 #define NR_REGEX ARRLEN(rules)
 
 static regex_t re[NR_REGEX] = {};
@@ -40,6 +55,7 @@ void init_regex() {
 
   for (i = 0; i < NR_REGEX; i ++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+    /* if parsing failed, print the failure information */
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
@@ -47,17 +63,29 @@ void init_regex() {
   }
 }
 
+/* using the bollow structure record the infomation of token 
+ * the length of str is limited in 32, means the most number we could record is 32
+*/
 typedef struct token {
   int type;
   char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
+/* nr_token indicates the number of tokens that have been identified */
 static int nr_token __attribute__((used))  = 0;
 
+
+/**
+ * @description: used to identified the tokens of expression which will be evaluated
+ * @param {char} *e
+ * @return {*}
+ * @use: 
+ */
 static bool make_token(char *e) {
   int position = 0;
   int i;
+  /* using pmatch to record the Byte offset from string's start to substring's start/end. */
   regmatch_t pmatch;
 
   nr_token = 0;
@@ -66,7 +94,9 @@ static bool make_token(char *e) {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        /* substr_start points to the address where the substring starts */
         char *substr_start = e + position;
+        /* substr_start is the length of substring */
         int substr_len = pmatch.rm_eo;
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
@@ -80,9 +110,10 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
+          case '+' : tokens[nr_token].type = '+';
           default: TODO();
         }
-
+        nr_token++;
         break;
       }
     }
