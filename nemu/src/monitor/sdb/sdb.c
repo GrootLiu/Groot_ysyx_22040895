f7eafb99f58b5f5b6d2466f6d242baa7d1abda63
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-02 20:22:33 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:33 up 20:04,  3 users,  load average: 0.19, 0.13, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -98,7 +98,7 @@ static int cmd_help(char *args) {
   if (arg == NULL) {
     /* no argument given */
     for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+      printf("%10s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
   }
   else {
