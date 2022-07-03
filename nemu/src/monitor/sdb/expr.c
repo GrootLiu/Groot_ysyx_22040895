1f8e37cb9429fd043b512c334cafb568bd55d6a1
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-03 14:04:20 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:20 up 1 day,  2:34,  3 users,  load average: 0.18, 0.11, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -5,15 +5,22 @@
  */
 #include <regex.h>
 
-/* 
- * token_type is an integer, 
+#include <assert.h>
+#include <stack.c>
+/*
+ * token_type is an integer,
  * we must ensure the integers corresponding to different types of token are not the same
  * the begining of tokens' number is 256
  * LP: Left parenthesis
  * RP: Right parenthsis
  */
-enum {
-  TK_NOTYPE = 256, TK_EQ, TK_LP, TK_RP, TK_NUM
+enum
+{
+  TK_NOTYPE = 256,
+  TK_EQ,
+  TK_LP,
+  TK_RP,
+  TK_NUM
   // TK_PLUS, TK_SUB, TK_MULT, TK_DIV,
 
   /* TODO: Add more token types */
@@ -21,28 +28,29 @@ enum {
 };
 
 /**
- * @description: in the framework code, 
+ * @description: in the framework code,
  *               a rule is a 2-tuple consisting of a regular expression and a token type
  *               rule = regex + token
  * @return {*}
- * @use: 
+ * @use:
  */
-static struct rule {
+static struct rule
+{
   const char *regex;
   int token_type;
 } rules[] = {
 
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
+    /* TODO: Add more rules.
+     * Pay attention to the precedence level of different rules.
+     */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\(", TK_LP},       // Left parenthesis
-  {"\\)", TK_RP},       // Right parenthesis
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ},        // equal
-  {"[0-9]+", TK_NUM}    // number
-  
+    {" +", TK_NOTYPE}, // spaces
+    {"\\(", TK_LP},    // Left parenthesis
+    {"\\)", TK_RP},    // Right parenthesis
+    {"\\+", '+'},      // plus
+    {"==", TK_EQ},     // equal
+    {"[0-9]+", TK_NUM} // number
+
 };
 
 /* NR_REGEX indicates the number of rules */
@@ -53,41 +61,45 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
+void init_regex()
+{
   int i;
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for (i = 0; i < NR_REGEX; i++)
+  {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
     /* if parsing failed, print the failure information */
-    if (ret != 0) {
+    if (ret != 0)
+    {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 }
 
-/* using the bollow structure record the infomation of token 
+/* using the bollow structure record the infomation of token
  * the length of str is limited in 32, means the most number we could record is 32
-*/
-typedef struct token {
+ */
+typedef struct token
+{
   int type;
   char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
 /* nr_token indicates the number of tokens that have been identified */
-static int nr_token __attribute__((used))  = 0;
-
+static int nr_token __attribute__((used)) = 0;
 
 /**
  * @description: used to identified the tokens of expression which will be evaluated
  * @param {char} *e
  * @return {*}
- * @use: 
+ * @use:
  */
-static bool make_token(char *e) {
+static bool make_token(char *e)
+{
   int position = 0;
   int i;
   /* using pmatch to record the Byte offset from string's start to substring's start/end. */
@@ -95,17 +107,20 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
+  while (e[position] != '\0')
+  {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i++)
+    {
       // printf(rules[i].regex)
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
+      {
         /* substr_start points to the address where the substring starts */
         char *substr_start = e + position;
         /* substr_start is the length of substring */
         int substr_len = pmatch.rm_eo;
 
-        // char *substr = 
+        // char *substr =
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
@@ -117,28 +132,39 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
-        switch (rules[i].token_type) {
-          case TK_NOTYPE : ;
-          case TK_LP : tokens[nr_token].type = TK_LP;
-          case TK_RP : tokens[nr_token].type = TK_RP;
-          case TK_NUM : 
-                        {
-                          tokens[nr_token].type = TK_NUM;
-                          strcpy(tokens[nr_token].str, substr_start);
-                        }                        
-          case '+' : tokens[nr_token].type = '+';
-          case '-' : tokens[nr_token].type = '-';
-          case '*' : tokens[nr_token].type = '*';
-          case '/' : tokens[nr_token].type = '/';
-          
-          default: TODO();
+        switch (rules[i].token_type)
+        {
+        case TK_NOTYPE:
+          nr_token--;
+        case TK_LP:
+          tokens[nr_token].type = TK_LP;
+        case TK_RP:
+          tokens[nr_token].type = TK_RP;
+        case TK_NUM:
+        {
+          tokens[nr_token].type = TK_NUM;
+          // 这里得用动态申请内存扩充str长度
+          assert(substr_len < 32);
+          strcpy(tokens[nr_token].str, substr_start);
+        }
+        case '+':
+          tokens[nr_token].type = '+';
+        case '-':
+          tokens[nr_token].type = '-';
+        case '*':
+          tokens[nr_token].type = '*';
+        case '/':
+          tokens[nr_token].type = '/';
+        default:
+          TODO();
         }
         nr_token++;
         break;
       }
     }
 
-    if (i == NR_REGEX) {
+    if (i == NR_REGEX)
+    {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
@@ -147,15 +173,82 @@ static bool make_token(char *e) {
   return true;
 }
 
-
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
+word_t expr(char *e, bool *success)
+{
+  if (!make_token(e))
+  {
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
+
   TODO();
+  /* p indicates the begining of the substring, q indicates the end of the substring
+   * now we init the p and q, give them 0 and 1, respectively
+   *
+   */
+  int p = 0, q = 1;
 
   return 0;
+}
+
+int eval(int start, int end)
+{
+  if (start > end)
+  {
+    /* Bad expression */
+    return -1;
+  }
+  else if (start == end)
+  {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+  }
+  else if (check_parentheses(start, end) == true)
+  {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(start + 1, end - 1);
+  }
+  else
+  {
+    /* We should do more things here. */
+  }
+  return 0;
+}
+
+bool check_parentheses(start, end)
+{
+  SqStack s;
+  InitStack(&s);
+  for (int i = start; i < end; i++)
+  {
+    if (tokens[i].type == TK_LP)
+    {
+      Push(&s, '(');
+    }
+    else if (tokens[i].type == TK_RP)
+    {
+      if (GetTop(s) == '(')
+      {
+        Pop(&s);
+      }
+      else
+      {
+        return false;
+      }      
+    }
+    else
+      continue;   
+  }
+  if (IsEmpty(s) == true)
+  {
+    return true;
+  }
+  else
+    return false;  
 }
