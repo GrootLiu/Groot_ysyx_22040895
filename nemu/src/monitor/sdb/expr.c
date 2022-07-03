5ad7c14afd60a50b6907b1881914636933a1b607
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-03 14:07:20 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:20 up 1 day,  2:37,  3 users,  load average: 0.10, 0.08, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -6,7 +6,89 @@
 #include <regex.h>
 
 #include <assert.h>
-#include <sdb/stack.c>
+
+// #include <stdio.h>
+#include <malloc.h>
+
+#define STACK_INT_SIZE 100
+#define STACKINCREMENT 10
+#define SElemType char
+
+bool InitStack(SqStack *S);
+bool GetTop(SqStack S);
+bool Push(SqStack *S, SElemType e);
+bool Pop(SqStack *S);
+bool IsEmpty(SqStack S);
+
+typedef struct
+{
+  /* stack base address */
+  SElemType *base; 
+  /* stack top address */
+  SElemType *top;  
+  int stacksize;
+} SqStack;
+
+/* Algorithmic description of basic operations */
+/* init a stack */
+bool InitStack(SqStack *S)
+{
+  S->base = (SElemType *)malloc(STACK_INT_SIZE * sizeof(SElemType)); //开辟新的空间
+  if (!S->base)
+  /* if stack creation failed, return 0*/
+    return 0; 
+  S->top = S->base;
+  S->stacksize = STACK_INT_SIZE;
+  return 1;
+}
+
+/* If the stack is not empty, return the top element of the stack and return true 
+ * otherwise return false 
+*/
+bool GetTop(SqStack S)
+{
+  if (IsEmpty(S))
+    return 0;
+  return *(S.top - 1);
+}
+
+/* Insert the element as the new top element of the stack */
+bool Push(SqStack *S, SElemType e)
+{
+  if (S->top - S->base >= S->stacksize)
+  /* If the stack is full, need to add space */
+  {
+    S->base = (SElemType *)realloc(S->base, (S->stacksize + STACKINCREMENT) * sizeof(SElemType));
+    if (!S->base)
+      return 0;
+    S->top = S->base + S->stacksize;
+    S->stacksize += STACKINCREMENT;
+  }
+  *(S->top++) = e;
+  return 1;
+}
+
+/* If the stack is not empty, delete the top element of the stack, return its value with e and return true, 
+ * otherwise return false 
+*/
+bool Pop(SqStack *S)
+{
+  if (IsEmpty(S))
+    return 0;
+  --S->top;
+  return 1;
+}
+
+bool IsEmpty(SqStack S)
+{
+    if (S.top == S.base)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+
 /*
  * token_type is an integer,
  * we must ensure the integers corresponding to different types of token are not the same
