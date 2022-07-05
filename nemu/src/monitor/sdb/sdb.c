6aa15706707b451eefce03af6b00e301c2d0349b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 18:22:50 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:22:50 up 2 days,  5:43,  3 users,  load average: 0.31, 0.18, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -155,7 +155,7 @@ static int cmd_x(char *args)
   /* addr is the beginning address we want to check */
   printf("%s\n", arg2);
   bool *success = false;
-  vaddr_t result = expr(arg2, success);
+  // vaddr_t result = expr(arg2, success);
   // printf("result: %lu\n", result);
   vaddr_t addr = strtoul(arg2, NULL, 16);
   
