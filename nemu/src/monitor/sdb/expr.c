4b0424220ce5e2f22ca8e9c090f7e03d73b9955e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 16:27:47 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:27:47 up 2 days,  3:48,  3 users,  load average: 0.06, 0.06, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -375,7 +375,7 @@ int find_priop(int start, int end)
   for (int i = start; i < end; i++)
   {
     printf("times=%d\n",i);
-    printf("op_pos[op_num].op=%c\n", op_pos[op_num].op);
+    printf("op_pos[op_num].op=%c\n", tokens[i].type);
     switch (tokens[i].type)
     {
     /* The token of the NOT operator is not a primary operator. */
