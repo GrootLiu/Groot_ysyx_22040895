fde268b756e3e370c20d5575fab578fdcf06fb4c
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-05 15:59:05 +0800

    > compile NEMU ysyx_22040895 刘澳 Linux groot-VirtualBox 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:59:05 up 2 days,  3:19,  3 users,  load average: 0.23, 0.15, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -298,6 +298,7 @@ uint32_t eval(int start, int end)
     /* analyse the main opertor*/
     // op = the position of 主运算符 in the token expression;
     int op = find_priop(start, end);
+    printf("op: %d\n", op);
     int val1 = eval(start, op - 1);
     int val2 = eval(op + 1, end);
     // printf("start=%d", start);
