2fdc9f01a32e20bd42b8a400ccfe7741c6b6f909
211220895-Liu Ao <1219671600@qq.com>
2022-08-08 11:07:52 +0800

    实现了HIT GOOD TRAP`

--- npc/vsrc/ysyx_22040895_regfile.v
+++ npc/vsrc/ysyx_22040895_regfile.v
@@ -1,4 +1,5 @@
 `include "/home/groot/ysyx-workbench/npc/include/define.v"
+import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
 
 module ysyx_22040895_regfile (input wire clk,
                               input wire rst,
@@ -27,6 +28,6 @@ module ysyx_22040895_regfile (input wire clk,
     assign rdata1_o_reg = (rst == `ysyx_22040895_RstEnable || raddr1_i_reg == 5'b0 || re1_i_reg == `ysyx_22040895_ReadDisable) ? 64'b0 : regs[raddr1_i_reg];
     // 寄存器读端口2
     assign rdata2_o_reg = (rst == `ysyx_22040895_RstEnable || raddr2_i_reg == 5'b0 || re2_i_reg == `ysyx_22040895_ReadDisable) ? 64'b0 : regs[raddr2_i_reg];
-    import "DPI-C" function void set_gpr_ptr(input logic [63:0] a []);
+    
     initial set_gpr_ptr(regs);  // rf为通用寄存器的二维数组变量
 endmodule //regfile
