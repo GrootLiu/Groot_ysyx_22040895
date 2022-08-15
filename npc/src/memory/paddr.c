90e11c2993b35217434154247dddf59295fc0247
211220895-Liu Ao <1219671600@qq.com>
2022-08-15 17:53:45 +0800

    在paddr.c中添加了对内存读写的操作

--- npc/src/memory/paddr.c
+++ npc/src/memory/paddr.c
@@ -1,7 +1,7 @@
 /*
  * @Author: Groot
  * @Date: 2022-07-14 22:36:28
- * @LastEditTime: 2022-08-14 11:52:21
+ * @LastEditTime: 2022-08-15 17:49:45
  * @LastEditors: Groot
  * @Description:
  * @FilePath: /ysyx-workbench/npc/src/memory/paddr.c
@@ -21,9 +21,42 @@
 #define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET)
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
-
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint8_t *guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+
+static inline uint64_t host_read(void *addr, int len)
+{
+	switch (len)
+	{
+	case 1:
+		return *(uint8_t *)addr;
+	case 2:
+		return *(uint16_t *)addr;
+	case 4:
+		return *(uint32_t *)addr;
+	case 8:
+		return *(uint64_t *)addr;
+	}
+}
+
+static inline void host_write(void *addr, int len, uint64_t data)
+{
+	switch (len)
+	{
+	case 1:
+		*(uint8_t *)addr = data;
+		return;
+	case 2:
+		*(uint16_t *)addr = data;
+		return;
+	case 4:
+		*(uint32_t *)addr = data;
+		return;
+	case 8:
+		*(uint64_t *)addr = data;
+		return;
+	}
+}
 
 int outOfBound(uint64_t addr)
 {
@@ -33,7 +66,16 @@ int outOfBound(uint64_t addr)
 	}
 	return 1;
 }
+static uint64_t pmem_read(paddr_t addr, int len)
+{
+	uint64_t ret = host_read(guest_to_host(addr), len);
+	return ret;
+}
 
+static void pmem_write(paddr_t addr, int len, uint64_t data)
+{
+	host_write(guest_to_host(addr), len, data);
+}
 uint32_t paddr_read(uint64_t addr)
 {
 	// printf("pc: %lx\n", addr);
--- npc/vsrc/ysyx_22040895_idu.v
+++ npc/vsrc/ysyx_22040895_idu.v
@@ -1,17 +1,17 @@
 `include "/home/groot/ysyx-workbench/npc/include/define.v"
 
 module ysyx_22040895_idu (input wire rst,
-            input wire[`ysyx_22040895_InstBus] inst_i_idu,
-            input wire[`ysyx_22040895_InstAddrBus] pc_i_idu,
-            output wire[`ysyx_22040895_OpCodeLength] opcode_o_idu,
-            output wire[`ysyx_22040895_func3Length] func3_o_idu,
-            output wire[`ysyx_22040895_func7Length] func7_o_idu,
-            output wire[`ysyx_22040895_imm1Length] imm1_o_idu,
-            output wire[`ysyx_22040895_imm2Length] imm2_o_idu,
-            output wire[`ysyx_22040895_RegAddrBus] rs1addr_o_idu,
-            output wire[`ysyx_22040895_RegAddrBus] rs2addr_o_idu,
-            output wire[`ysyx_22040895_RegAddrBus] rdaddr_o_idu,
-            output wire[`ysyx_22040895_InstAddrBus] pc_o_idu);
+                          input wire[`ysyx_22040895_InstBus] inst_i_idu,
+                          input wire[`ysyx_22040895_InstAddrBus] pc_i_idu,
+                          output wire[`ysyx_22040895_OpCodeLength] opcode_o_idu,
+                          output wire[`ysyx_22040895_func3Length] func3_o_idu,
+                          output wire[`ysyx_22040895_func7Length] func7_o_idu,
+                          output wire[`ysyx_22040895_imm1Length] imm1_o_idu,
+                          output wire[`ysyx_22040895_imm2Length] imm2_o_idu,
+                          output wire[`ysyx_22040895_RegAddrBus] rs1addr_o_idu,
+                          output wire[`ysyx_22040895_RegAddrBus] rs2addr_o_idu,
+                          output wire[`ysyx_22040895_RegAddrBus] rdaddr_o_idu,
+                          output wire[`ysyx_22040895_InstAddrBus] pc_o_idu);
     
     wire branch;
     
@@ -23,6 +23,8 @@ module ysyx_22040895_idu (input wire rst,
     wire bne;
     wire jal;
     
+    wire store;
+    
     assign beq  = (opcode_o_idu == 7'b1100011) & (func3_o_idu == 3'b000);
     assign bge  = (opcode_o_idu == 7'b1100011) & (func3_o_idu == 3'b101);
     assign bgeu = (opcode_o_idu == 7'b1100011) & (func3_o_idu == 3'b111);
@@ -31,19 +33,23 @@ module ysyx_22040895_idu (input wire rst,
     assign bne  = (opcode_o_idu == 7'b1100011) & (func3_o_idu == 3'b001);
     assign jal  = (opcode_o_idu == 7'b1101111);
     
+    assign store = (opcode_o_idu == 7'b0100011);
+    
     assign branch = beq | bge | bgeu | blt | bltu | bne;
     
     wire[11:0] branch_offset = {inst_i_idu[11:8], inst_i_idu[30:25], inst_i_idu[7], inst_i_idu[31]};
-    wire[19:0] jal_offset   = {inst_i_idu[31], inst_i_idu[19:12], inst_i_idu[20], inst_i_idu[30:21]};
+    wire[19:0] jal_offset    = {inst_i_idu[31], inst_i_idu[19:12], inst_i_idu[20], inst_i_idu[30:21]};
+    wire[11:0] store_offset  = {inst_i_idu[31:25], inst_i_idu[11:7]};
     
-    assign pc_o_idu      = (rst == `ysyx_22040895_RstEnable) ? 64'b0 : pc_i_idu;
-    assign opcode_o_idu  = (rst == `ysyx_22040895_RstEnable) ? 7'b0 : inst_i_idu[6:0];
-    assign func3_o_idu   = (rst == `ysyx_22040895_RstEnable) ? 3'b0 : inst_i_idu[14:12];
-    assign func7_o_idu   = (rst == `ysyx_22040895_RstEnable) ? 7'b0 : inst_i_idu[31:25];
-    assign imm1_o_idu    = (rst == `ysyx_22040895_RstEnable) ? 12'b0 :
-                           (branch == 1'b1)    ? branch_offset : inst_i_idu[31:20];
-    assign imm2_o_idu    = (rst == `ysyx_22040895_RstEnable) ? 20'b0 : 
-                           (jal == 1'b1)      ? jal_offset : inst_i_idu[31:12];
+    assign pc_o_idu     = (rst == `ysyx_22040895_RstEnable) ? 64'b0 : pc_i_idu;
+    assign opcode_o_idu = (rst == `ysyx_22040895_RstEnable) ? 7'b0 : inst_i_idu[6:0];
+    assign func3_o_idu  = (rst == `ysyx_22040895_RstEnable) ? 3'b0 : inst_i_idu[14:12];
+    assign func7_o_idu  = (rst == `ysyx_22040895_RstEnable) ? 7'b0 : inst_i_idu[31:25];
+    assign imm1_o_idu = (rst == `ysyx_22040895_RstEnable) ? 12'b0 :
+    					(branch == 1'b1) ? branch_offset :
+    					(store == 1'b1)  ? store_offset : inst_i_idu[31:20];
+    assign imm2_o_idu = (rst == `ysyx_22040895_RstEnable) ? 20'b0 :
+    					(jal == 1'b1)    ? jal_offset : inst_i_idu[31:12];
     assign rs1addr_o_idu = (rst == `ysyx_22040895_RstEnable) ? 5'b0 : inst_i_idu[19:15];
     assign rs2addr_o_idu = (rst == `ysyx_22040895_RstEnable) ? 5'b0 : inst_i_idu[24:20];
     assign rdaddr_o_idu  = (rst == `ysyx_22040895_RstEnable) ? 5'b0 : inst_i_idu[11:7];
