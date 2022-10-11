da5ec9b8a9697de25830ca07060e018d2508d6d7
211220895-Liu Ao <1219671600@qq.com>
2022-10-11 22:24:56 +0800

    实现了nemu运行模拟的字符版本的红白机 实现了dtrace，并且能够通过make menuconfig进行控制

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -1,3 +1,12 @@
+/*
+ * @Author: Groot
+ * @Date: 2022-04-06 19:26:19
+ * @LastEditTime: 2022-10-11 22:21:13
+ * @LastEditors: Groot
+ * @Description:
+ * @FilePath: /ysyx-workbench/nemu/src/device/io/map.c
+ * 版权声明
+ */
 #include <isa.h>
 #include <memory/host.h>
 #include <memory/vaddr.h>
@@ -8,48 +17,70 @@
 static uint8_t *io_space = NULL;
 static uint8_t *p_space = NULL;
 
-uint8_t* new_space(int size) {
-  uint8_t *p = p_space;
-  // page aligned;
-  size = (size + (PAGE_SIZE - 1)) & ~PAGE_MASK;
-  p_space += size;
-  assert(p_space - io_space < IO_SPACE_MAX);
-  return p;
+uint8_t *new_space(int size)
+{
+	uint8_t *p = p_space;
+	// page aligned;
+	size = (size + (PAGE_SIZE - 1)) & ~PAGE_MASK;
+	p_space += size;
+	assert(p_space - io_space < IO_SPACE_MAX);
+	return p;
 }
 
-static void check_bound(IOMap *map, paddr_t addr) {
-  if (map == NULL) {
-    Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
-  } else {
-    Assert(addr <= map->high && addr >= map->low,
-        "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
-        addr, map->name, map->low, map->high, cpu.pc);
-  }
+static void check_bound(IOMap *map, paddr_t addr)
+{
+	if (map == NULL)
+	{
+		Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
+	}
+	else
+	{
+		Assert(addr <= map->high && addr >= map->low,
+			   "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+			   addr, map->name, map->low, map->high, cpu.pc);
+	}
 }
 
-static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write) {
-  if (c != NULL) { c(offset, len, is_write); }
+static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write)
+{
+	if (c != NULL)
+	{
+		c(offset, len, is_write);
+	}
 }
 
-void init_map() {
-  io_space = malloc(IO_SPACE_MAX);
-  assert(io_space);
-  p_space = io_space;
+void init_map()
+{
+	io_space = malloc(IO_SPACE_MAX);
+	assert(io_space);
+	p_space = io_space;
 }
 
-word_t map_read(paddr_t addr, int len, IOMap *map) {
-  assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
-  paddr_t offset = addr - map->low;
-  invoke_callback(map->callback, offset, len, false); // prepare data to read
-  word_t ret = host_read(map->space + offset, len);
-  return ret;
+word_t map_read(paddr_t addr, int len, IOMap *map)
+{
+	assert(len >= 1 && len <= 8);
+	check_bound(map, addr);
+	paddr_t offset = addr - map->low;
+	invoke_callback(map->callback, offset, len, false); // prepare data to read
+
+#ifdef CONFIG_DTRACE
+	// 在这个地方进行dtrace
+	log_write("Device %s is used!\n", map->name);
+#endif
+
+	word_t ret = host_read(map->space + offset, len);
+	return ret;
 }
 
-void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
-  assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
-  paddr_t offset = addr - map->low;
-  host_write(map->space + offset, len, data);
-  invoke_callback(map->callback, offset, len, true);
+void map_write(paddr_t addr, int len, word_t data, IOMap *map)
+{
+	assert(len >= 1 && len <= 8);
+	check_bound(map, addr);
+	paddr_t offset = addr - map->low;
+	host_write(map->space + offset, len, data);
+	invoke_callback(map->callback, offset, len, true);
+#ifdef CONFIG_DTRACE
+	// 在这个地方进行dtrace
+	log_write("Device %s is used!\n", map->name);
+#endif
 }
